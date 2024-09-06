32107fc1c6d6e575dc9806846641eecf2aeec7f8
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-09-06 17:19:48 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import { useState } from "react";
 
 const App = () => {
   const [number, setNumber] = useState(0);
-// increment
+  // increment
   function handleIncrement() {
     setNumber(number + 1);
   }
-// increment
+  // increment
   function handleDecrement() {
     setNumber(number - 1);
   }
   return (
     <div className="h-screen border2 border-red-500 flex justify-center items-center">
       <div className="w-96 p-10 rounded-lg mx-auto bg-orange-200 flex flex-row justify-between items-center">
+        {/* increment */}
         <button
           onClick={handleDecrement}
           className="text-white bg-red-500 hover:bg-red-400 text-xl font-semibold px-10 py-4 rounded-md"
@@ -20,6 +21,7 @@ const App = () => {
           -
         </button>
         <p className="text-4xl font-bold text-center">{number}</p>
+        {/* Decrement */}
         <button
           onClick={handleIncrement}
           className="text-white bg-green-500 hover:bg-green-400 text-xl font-semibold px-10 py-4 rounded-md"
@@ -29,6 +31,6 @@ const App = () => {
       </div>
     </div>
   );
-}
+};
 
 export default App;
